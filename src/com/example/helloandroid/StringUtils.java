2b60ade7302a883c483607657dcf1446256170d9
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-02-26 14:56:18 +0900

    Add StringUtils

---
+++ src/com/example/helloandroid/StringUtils.java
@@ -0,0 +1,16 @@
+package com.example.helloandroid;
+
+public class StringUtils {
+    public static boolean isBlank(CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
